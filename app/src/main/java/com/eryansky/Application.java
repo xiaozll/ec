d0e7fbdc214be1c0e4a77756934c87896ef14498
eryan <eryanwcp@gmail.com>
2019-02-01 17:31:46 +0800

    初始化

--- app/src/main/java/com/eryansky/Application.java
+++ app/src/main/java/com/eryansky/Application.java
@@ -1,28 +1,33 @@
 package com.eryansky;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-//@ComponentScan(value = {"com.eryansky.j2cache.autoconfigure",
-//        "com.eryansky.common.spring",
-//        "com.eryansky.configure",
-//        "com.eryansky.modules.**.task",
-//        "com.eryansky.modules.**.service",
-//        "com.eryansky.modules.**.quartz",
-//        "com.eryansky.modules.**.web"
-//})
-@EnableTransactionManagement
-@SpringBootApplication(exclude={MybatisAutoConfiguration.class, FreeMarkerAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+        scanBasePackages = {"com.eryansky.j2cache.autoconfigure",
+                "com.eryansky.common.spring",
+                "com.eryansky.configure",
+                "com.eryansky.modules.**.task",
+                "com.eryansky.modules.**.service",
+                "com.eryansky.modules.**.quartz",
+                "com.eryansky.modules.**.web"
+        },
+        exclude = {MybatisAutoConfiguration.class,
+                FreeMarkerAutoConfiguration.class,
+//                DataSourceAutoConfiguration.class,
+//                DruidDataSourceAutoConfigure.class,
+                DataSourceTransactionManagerAutoConfiguration.class
+        })
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {

6bb3cc9c50b4ee7afb8028cc289db1bb1f3859f3
eryan <eryanwcp@gmail.com>
2019-11-15 12:13:06 +0800

    登录拦截日志

---
+++ app/src/main/java/com/eryansky/modules/sys/utils/PostUtils.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2012-2014 http://www.eryansky.com
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.eryansky.modules.sys.utils;
+
+import com.eryansky.common.spring.SpringContextHolder;
+import com.eryansky.common.utils.StringUtils;
+import com.eryansky.modules.sys.mapper.Post;
+import com.eryansky.modules.sys.service.PostService;
+
+/**
+ * @author 尔演&Eryan eryanwcp@gmail.com
+ * @date 2014-11-25
+ */
+public class PostUtils {
+
+
+    /**
+     * 静态内部类，延迟加载，懒汉式，线程安全的单例模式
+     */
+    public static final class Static {
+        private static PostService postService = SpringContextHolder.getBean(PostService.class);
+    }
+
+    /**
+     * 根据ID查找
+     * @param postId 岗位ID
+     * @return
+     */
+    public static Post get(String postId) {
+        if (StringUtils.isBlank(postId)) {
+            return null;
+        }
+        return Static.postService.get(postId);
+    }
+
+    /**
+     * 根据ID查找名称
+     * @param postId
+     * @return
+     */
+    public static String getPostName(String postId) {
+        Post post = get(postId);
+        if (post != null) {
+            return post.getName();
+        }
+        return null;
+    }
+
+}

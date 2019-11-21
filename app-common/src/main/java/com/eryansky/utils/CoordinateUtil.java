f57809d87c058a2997cb610a96ffc9343a32389e
eryan <eryanwcp@gmail.com>
2019-11-21 18:26:22 +0800

    优化升级

--- app-common/src/main/java/com/eryansky/utils/CoordinateUtil.java
+++ app-common/src/main/java/com/eryansky/utils/CoordinateUtil.java
@@ -56,7 +56,7 @@ public class CoordinateUtil {
 	}
 
 	private static double transLat(double x,double y){
-		double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(x > 0 ? x : -x);
+		double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(x > 0 ? x:-x);
 		ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
 		ret += (20.0 * Math.sin(y * pi) + 40.0 * Math.sin(y / 3.0 * pi)) * 2.0 / 3.0;
 		ret += (160.0 * Math.sin(y / 12.0 * pi) + 320 * Math.sin(y * pi / 30.0)) * 2.0 / 3.0;
@@ -64,7 +64,7 @@ public class CoordinateUtil {
 	}
 
 	private static double transLon(double x,double y){
-		double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(x > 0 ? x : -x);
+		double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(x > 0 ? x:-x);
 		ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
 		ret += (20.0 * Math.sin(x * pi) + 40.0 * Math.sin(x / 3.0 * pi)) * 2.0 / 3.0;
 		ret += (150.0 * Math.sin(x / 12.0 * pi) + 300.0 * Math.sin(x / 30.0 * pi)) * 2.0 / 3.0;
@@ -145,7 +145,46 @@ public class CoordinateUtil {
 		return wgLoc;
 	}
 
-	public static double EARTH_RADIUS = 6378137;
+
+	/**
+	 * 坐标转换，腾讯地图转换成百度地图坐标
+	 * @param lon 腾讯经度
+	 * @param lat 腾讯纬度
+	 * @return 返回结果：经度,纬度
+	 */
+	public static double[] map_tx2bd(double lon, double lat){
+		double bd_lat;
+		double bd_lon;
+		double x_pi=3.14159265358979324;
+		double x = lon, y = lat;
+		double z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi);
+		double theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi);
+		bd_lon = z * Math.cos(theta) + 0.0065;
+		bd_lat = z * Math.sin(theta) + 0.006;
+
+		return new double[]{bd_lon,bd_lat};
+	}
+
+
+	/**
+	 * 坐标转换，百度地图坐标转换成腾讯地图坐标
+	 * @param lon  百度坐标经度
+	 * @param lat  百度坐标纬度
+	 * @return 返回结果：经度,纬度
+	 */
+	public static double[] map_bd2tx(double lon, double lat){
+		double tx_lat;
+		double tx_lon;
+		double x_pi=3.14159265358979324;
+		double x = lon - 0.0065, y = lat - 0.006;
+		double z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_pi);
+		double theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_pi);
+		tx_lon = z * Math.cos(theta);
+		tx_lat = z * Math.sin(theta);
+		return new double[]{tx_lon,tx_lat};
+	}
+
+	private static double EARTH_RADIUS = 6378137;
 	private static double rad(double d) {
 		return d * Math.PI / 180.0;
 	}

2d2e4b46e965f4ee2321a6e9ff8d99b009046831
eryan <eryanwcp@gmail.com>
2020-02-27 14:15:42 +0800

    电话号码验证

--- common/src/main/java/com/eryansky/common/utils/SysUtils.java
+++ common/src/main/java/com/eryansky/common/utils/SysUtils.java
@@ -1198,6 +1198,48 @@ public class SysUtils {
 		}
 	}
 
+	/**
+	 * 手机号验证
+	 * @param  str
+	 * @return 验证通过返回true
+	 */
+	public static boolean isMobile(final String str) {
+		if(null == str){
+			return false;
+		}
+		Pattern p = null;
+		Matcher m = null;
+		boolean b = false;
+		p = Pattern.compile("^[1][3,4,5,7,8][0-9]{9}$"); // 验证手机号
+		m = p.matcher(str);
+		b = m.matches();
+		return b;
+	}
+	/**
+	 * 电话号码验证
+	 * @param  str
+	 * @return 验证通过返回true
+	 */
+	public static boolean isPhone(final String str) {
+		if(null == str){
+			return false;
+		}
+		Pattern p1 = null, p2 = null;
+		Matcher m = null;
+		boolean b = false;
+		p1 = Pattern.compile("^[0][1-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
+		p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
+		if (str.length() > 9) {
+			m = p1.matcher(str);
+			b = m.matches();
+		} else {
+			m = p2.matcher(str);
+			b = m.matches();
+		}
+		return b;
+	}
+
+
 	public static String fillzero(String str, int len) {
 		if (str == null) {
 			return "";

53034320f265c4e607e091518f4f2edd22191237
eryan <eryanwcp@gmail.com>
2020-06-17 17:10:56 +0800

    list分组

--- common/src/main/java/com/eryansky/common/utils/collections/Collections3.java
+++ common/src/main/java/com/eryansky/common/utils/collections/Collections3.java
@@ -264,4 +264,25 @@ public class Collections3 {
 		}
 		return a.subList(fromIndex, toIndex);
 	}
+
+	/**
+	 * List分组
+	 * @param list
+	 * @param groupSize 分组大小
+	 * @return
+	 */
+	public static List<List> splitList(List list , int groupSize){
+		int length = list.size();
+		// 计算可以分成多少组
+		int num = ( length + groupSize - 1 )/groupSize ; // TODO
+		List<List> newList = new ArrayList<>(num);
+		for (int i = 0; i < num; i++) {
+			// 开始位置
+			int fromIndex = i * groupSize;
+			// 结束位置
+			int toIndex = (i+1) * groupSize < length ? ( i+1 ) * groupSize : length ;
+			newList.add(list.subList(fromIndex,toIndex)) ;
+		}
+		return  newList ;
+	}
 }

eb4c0bc6b37e7e45836faf21c1c361a7312f70ad
eryan <eryanwcp@gmail.com>
2020-07-27 13:54:24 +0800

    线程池优化

--- app-common/src/main/java/com/eryansky/configure/AopConfigurer.java
+++ app-common/src/main/java/com/eryansky/configure/AopConfigurer.java
@@ -2,10 +2,12 @@ package com.eryansky.configure;
 
 import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
@@ -20,6 +22,18 @@ public class AopConfigurer implements AsyncConfigurer {
 
     private static Logger logger = LoggerFactory.getLogger(AopConfigurer.class);
 
+//    @Value("${thread.pool.corePoolSize:10}")
+//    private int corePoolSize;
+//
+//    @Value("${thread.pool.maxPoolSize:20}")
+//    private int maxPoolSize;
+//
+//    @Value("${thread.pool.keepAliveSeconds:60}")
+//    private int keepAliveSeconds;
+//
+//    @Value("${thread.pool.queueCapacity:1024}")
+//    private int queueCapacity;
+
     @Override
     public Executor getAsyncExecutor() {
         //线程池
@@ -27,23 +41,18 @@ public class AopConfigurer implements AsyncConfigurer {
         //核心线程池数量，方法: 返回可用处理器的Java虚拟机的数量。
         int processors = Runtime.getRuntime().availableProcessors();
         executor.setCorePoolSize(processors);
-        //最大线程数量
-        executor.setMaxPoolSize(processors * 5);
-        //线程池的队列容量
-        executor.setQueueCapacity(processors * 100);
+        executor.setMaxPoolSize(processors * 5);//最大线程数量
+        executor.setQueueCapacity(processors * 100);//线程池的队列容量
+        executor.setRejectedExecutionHandler((Runnable r, ThreadPoolExecutor exe) -> {
+            logger.warn("当前任务线程池队列已满.");
+        });
         executor.initialize();
         return executor;
     }
 
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new AsyncUncaughtExceptionHandler() {
-            @Override
-            public void handleUncaughtException(Throwable throwable, Method method, Object... objects) {
-                logger.error(method.getName() +":"+throwable.getMessage(), throwable);
-            }
-
-        };
+        return (throwable, method, objects) -> logger.error("线程池执行任务发生未知异常,"+method.getName() +":"+throwable.getMessage(), throwable);
     }
 
 }

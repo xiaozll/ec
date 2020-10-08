985450fac262e275d20d05323668de7d30e6e17a
eryan <eryanwcp@gmail.com>
2020-10-08 12:51:57 +0800

    序列号生成器支持多分类划分

--- app/src/test/java/com/eryansky/ApplicationTests.java
+++ app/src/test/java/com/eryansky/ApplicationTests.java
@@ -28,93 +28,93 @@ import static org.mybatis.dynamic.sql.select.SelectDSL.select;
 @SpringBootTest
 public class ApplicationTests {
 
-	@Autowired
-	private ConfigService configService;
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private RoleService roleService;
-	@Autowired
-	private PostService postService;
-	@Autowired
-	private VersionLogService versionLogService;
+    @Autowired
+    private ConfigService configService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RoleService roleService;
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private VersionLogService versionLogService;
 
-	@Test
-	public void contextLoads() {
-		userService.aop();
-	}
-
-
-	@Test
-	public void addUserRoleAndPost() {
-		String userId = "1eb60440083945bb84394b7b1cc77414";
-		String postCode = "post1";
-		String roleCode = "role1";
-		System.out.println(JsonMapper.toJsonString(postService.findPostsByUserId(userId)));
-		UserUtils.addUserOrganPost(userId,postCode);
-		System.out.println(JsonMapper.toJsonString(postService.findPostsByUserId(userId)));
-
-		System.out.println(JsonMapper.toJsonString(roleService.findRolesByUserId(userId)));
-		UserUtils.addUserRole(userId,roleCode);
-		System.out.println(JsonMapper.toJsonString(roleService.findRolesByUserId(userId)));
-	}
+    @Test
+    public void contextLoads() {
+        userService.aop();
+    }
 
 
-	@Test
-	public void e() {
-		System.out.println(Encrypt.e("1"));
-	}
+    @Test
+    public void addUserRoleAndPost() {
+        String userId = "1eb60440083945bb84394b7b1cc77414";
+        String postCode = "post1";
+        String roleCode = "role1";
+        System.out.println(JsonMapper.toJsonString(postService.findPostsByUserId(userId)));
+        UserUtils.addUserOrganPost(userId, postCode);
+        System.out.println(JsonMapper.toJsonString(postService.findPostsByUserId(userId)));
+
+        System.out.println(JsonMapper.toJsonString(roleService.findRolesByUserId(userId)));
+        UserUtils.addUserRole(userId, roleCode);
+        System.out.println(JsonMapper.toJsonString(roleService.findRolesByUserId(userId)));
+    }
 
 
-	@Test
-	public void generateSerialNumberByModelCode() {
-		String moduleCode = "A01";
-		Map<String,String> params0 = Maps.newHashMap();
-		params0.put("param1","");
-		params0.put("param2","");
-		String customCategory0 = null;
-		for(int i=1;i<10;i++){
-			int finalI = i;
-			new Thread(() ->{
-				System.out.println(Thread.currentThread().getName() +" 0-"+ finalI +" " +SystemSerialNumberUtils.generateSerialNumberByModelCode(moduleCode,customCategory0,params0));
-			}).start();
-		}
-
-		Map<String,String> params1 = Maps.newHashMap();
-		params1.put("param1","A01");
-		params1.put("param2","A01");
-		String customCategory1 = "A01";
-		for(int i=1;i<10;i++){
-			int finalI = i;
-			new Thread(() ->{
-				System.out.println(Thread.currentThread().getName() +" 1-"+ finalI +" " +SystemSerialNumberUtils.generateSerialNumberByModelCode(moduleCode,customCategory1,params1));
-			}).start();
-		}
-		Map<String,String> params2 = Maps.newHashMap();
-		params2.put("param1","B02");
-		params2.put("param2","B02");
-		String customCategory2 = "B02";
-		for(int i=1;i<10;i++){
-			int finalI = i;
-			new Thread(() ->{
-				System.out.println(Thread.currentThread().getName() +" 2-"+ finalI +" " +SystemSerialNumberUtils.generateSerialNumberByModelCode(moduleCode,customCategory2,params2));
-			}).start();
-		}
-
-		ThreadUtils.sleep(30*1000);
-
-	}
+    @Test
+    public void e() {
+        System.out.println(Encrypt.e("1"));
+    }
 
 
-	@Test
-	public void testSelectByExample() {
-		SelectStatementProvider selectStatement = select(data.allColumns())
-				.from(data,"a")
-				.where(app,isEqualTo("1"))
-				.build()
-				.render(RenderingStrategies.MYBATIS3);
+    @Test
+    public void generateSerialNumberByModelCode() {
+        String moduleCode = "A01";
+        Map<String, String> params0 = Maps.newHashMap();
+        params0.put("param1", "");
+        params0.put("param2", "");
+        String customCategory0 = null;
+        for (int i = 1; i < 10; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " 0-" + finalI + " " + SystemSerialNumberUtils.generateSerialNumberByModelCode(moduleCode, customCategory0, params0));
+            }).start();
+        }
 
-		List<VersionLog> rows = versionLogService.selectMany(selectStatement);
-		System.out.println(JsonMapper.toJsonString(rows));
-	}
+        Map<String, String> params1 = Maps.newHashMap();
+        params1.put("param1", "A01");
+        params1.put("param2", "A01");
+        String customCategory1 = "A01";
+        for (int i = 1; i < 10; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " 1-" + finalI + " " + SystemSerialNumberUtils.generateSerialNumberByModelCode(moduleCode, customCategory1, params1));
+            }).start();
+        }
+        Map<String, String> params2 = Maps.newHashMap();
+        params2.put("param1", "B02");
+        params2.put("param2", "B02");
+        String customCategory2 = "B02";
+        for (int i = 1; i < 10; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " 2-" + finalI + " " + SystemSerialNumberUtils.generateSerialNumberByModelCode(moduleCode, customCategory2, params2));
+            }).start();
+        }
+
+        ThreadUtils.sleep(30 * 1000);
+
+    }
+
+
+    @Test
+    public void testSelectByExample() {
+        SelectStatementProvider selectStatement = select(data.allColumns())
+                .from(data, "a")
+                .where(app, isEqualTo("1"))
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+
+        List<VersionLog> rows = versionLogService.selectMany(selectStatement);
+        System.out.println(JsonMapper.toJsonString(rows));
+    }
 }

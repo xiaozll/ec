ab4de6d43bfd13b2bf4904956130f5b2bac48196
eryan <eryanwcp@gmail.com>
2020-10-23 14:37:25 +0800

    重命名

--- app-common/src/main/java/com/eryansky/modules/sys/web/mobile/MobileIndexController.java
+++ app-common/src/main/java/com/eryansky/modules/sys/web/mobile/MobileIndexController.java
@@ -44,6 +44,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -198,10 +199,85 @@ public class MobileIndexController extends SimpleController {
             if (!diskFile.exists() || !diskFile.canRead()) {
                 throw fileNotFoldException;
             }
-
             String filename = file.getName();
-            WebUtils.setDownloadableHeader(request, response, filename);
-            DownloadFileUtils.downRangeFile(diskFile,response,request);
+            long fileLength = diskFile.length();// 记录文件大小
+            long pastLength = 0;// 记录已下载文件大小
+            long toLength = 0;// 记录客户端需要下载的字节段的最后一个字节偏移量（比如bytes=27000-39000，则这个值是为39000）
+            long contentLength = 0;// 客户端请求的字节总量
+            String rangeBytes = "";// 记录客户端传来的形如“bytes=27000-”或者“bytes=27000-39000”的内容
+
+            // ETag header
+            // The ETag is contentLength + lastModified
+            response.setHeader("ETag", "W/\"" + fileLength + "-" + diskFile.lastModified() + "\"");
+            // Last-Modified header
+            response.setHeader("Last-Modified", new Date(diskFile.lastModified()).toString());
+
+            if (request.getHeader("Range") != null) {// 客户端请求的下载的文件块的开始字节
+                response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+                logger.debug("request.getHeader(\"Range\")=" + request.getHeader("Range"));
+                rangeBytes = request.getHeader("Range").replaceAll("bytes=", "");
+                if (rangeBytes.indexOf('-') == rangeBytes.length() - 1) {// bytes=969998336-
+                    rangeBytes = rangeBytes.substring(0, rangeBytes.indexOf('-'));
+                    pastLength = Long.parseLong(rangeBytes.trim());
+                    toLength = fileLength - 1;
+                } else {// bytes=1275856879-1275877358
+                    String temp0 = rangeBytes.substring(0, rangeBytes.indexOf('-'));
+                    String temp2 = rangeBytes.substring(
+                            rangeBytes.indexOf('-') + 1, rangeBytes.length());
+                    // bytes=1275856879-1275877358，从第 1275856879个字节开始下载
+                    pastLength = Long.parseLong(temp0.trim());
+                    // bytes=1275856879-1275877358，到第 1275877358 个字节结束
+                    toLength = Long.parseLong(temp2);
+                }
+            } else {// 从开始进行下载
+                toLength = fileLength - 1;
+            }
+            // 客户端请求的是1275856879-1275877358 之间的字节
+            contentLength = toLength - pastLength + 1;
+            if (contentLength < Integer.MAX_VALUE) {
+                response.setContentLength((int) contentLength);
+            } else {
+                // Set the content-length as String to be able to use a long
+                response.setHeader("content-length", "" + contentLength);
+            }
+            String contentType = AppUtils.getServletContext().getMimeType(filename);
+            if (null != contentType) {
+                response.setContentType(contentType);
+            } else {
+                response.setContentType("application/x-download");
+            }
+
+            // 告诉客户端允许断点续传多线程连接下载,响应的格式是:Accept-Ranges: bytes
+            response.setHeader("Accept-Ranges", "bytes");
+            // 必须先设置content-length再设置header
+            response.addHeader("Content-Range", "bytes " + pastLength + "-" + toLength + "/" + fileLength);
+            int bufferSize = 2048;
+            response.setBufferSize(bufferSize);
+
+            InputStream istream = null;
+            OutputStream os = null;
+            try {
+                WebUtils.setDownloadableHeader(request, response, filename);
+                os = response.getOutputStream();
+                istream = new BufferedInputStream(new FileInputStream(diskFile), bufferSize);
+                try {
+                    IoUtils.copy(istream, os, pastLength, toLength);
+                } catch (IOException ie) {
+                    /**
+                     * 在写数据的时候， 对于 ClientAbortException 之类的异常，
+                     * 是因为客户端取消了下载，而服务器端继续向浏览器写入数据时， 抛出这个异常，这个是正常的。
+                     * 尤其是对于迅雷这种吸血的客户端软件， 明明已经有一个线程在读取 bytes=1275856879-1275877358，
+                     * 如果短时间内没有读取完毕，迅雷会再启第二个、第三个。。。线程来读取相同的字节段， 直到有一个线程读取完毕，迅雷会 KILL
+                     * 掉其他正在下载同一字节段的线程， 强行中止字节读出，造成服务器抛 ClientAbortException。
+                     * 所以，我们忽略这种异常
+                     */
+                    // ignore
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            } finally {
+                IoUtils.closeSilently(istream);
+            }
         } catch (Exception e) {
             logger.error(e.getMessage(),e);
             throw new ActionException(e);

51a995080b13f5355c6c993dae4db32682761b7c
eryan <eryanwcp@gmail.com>
2021-03-26 09:03:48 +0800

    Spring boot 2.4.4

--- app-common/src/main/java/com/eryansky/core/excelTools/CsvUtils.java
+++ app-common/src/main/java/com/eryansky/core/excelTools/CsvUtils.java
@@ -47,6 +47,9 @@ public class CsvUtils {
      * @param outputStream 创建的csv文件路径
      **/
     public static void writeCsv(String[] headers, List<Object[]> data, OutputStream outputStream) throws IOException {
+        // 写入bom, 防止中文乱码
+        byte[] bytes = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
+        outputStream.write(bytes);
         //初始化csvformat
         CSVFormat formator = CSVFormat.DEFAULT.withRecordSeparator("\n");
         //创建FileWriter对象
@@ -80,11 +83,50 @@ public class CsvUtils {
         WebUtils.setDownloadableHeader(request, response, sFileName);
         response.setHeader("Connection", "close");
         response.setHeader("Content-Type", "text/csv");
-
+        OutputStream outputStream = response.getOutputStream();
+        // 写入bom, 防止中文乱码
+        byte[] bytes = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
+        outputStream.write(bytes);
         //初始化csvformat
         CSVFormat formator = CSVFormat.DEFAULT.withRecordSeparator("\n");
         //创建FileWriter对象
-        Writer outputStreamWriter = new OutputStreamWriter(response.getOutputStream());
+        Writer outputStreamWriter = new OutputStreamWriter(outputStream);
+        //创建CSVPrinter对象
+        CSVPrinter printer = new CSVPrinter(outputStreamWriter, formator);
+        //写入列头数据
+        printer.printRecord(headers);
+        if (null != data) {
+            //循环写入数据
+            for (Object[] lineData : data) {
+                printer.printRecord(lineData);
+            }
+        }
+        outputStreamWriter.flush();
+        printer.close(true);
+        outputStreamWriter.close();
+    }
+
+    /**
+     * 写入csv文件
+     * @param title  文件名
+     * @param headers  列头
+     * @param data     数据内容
+     * @param request
+     * @param response
+     **/
+    public static void exportToCsv(String title, String[] headers, List<Object[]> data, HttpServletRequest request, HttpServletResponse response, String charsetName) throws IOException {
+        String sFileName = title + ".csv";
+        WebUtils.setDownloadableHeader(request, response, sFileName);
+        response.setHeader("Connection", "close");
+        response.setHeader("Content-Type", "text/csv");
+        OutputStream outputStream = response.getOutputStream();
+        // 写入bom, 防止中文乱码
+        byte[] bytes = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
+        outputStream.write(bytes);
+        //初始化csvformat
+        CSVFormat formator = CSVFormat.DEFAULT.withRecordSeparator("\n");
+        //创建FileWriter对象
+        Writer outputStreamWriter = new OutputStreamWriter(outputStream,charsetName);
         //创建CSVPrinter对象
         CSVPrinter printer = new CSVPrinter(outputStreamWriter, formator);
         //写入列头数据

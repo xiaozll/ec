d082a6f9fd02b6c10c044ed1d6a18a99a74368b8
eryan <eryanwcp@gmail.com>
2021-11-30 14:39:56 +0800

    20211129 1、动态加密登录密码 2、XSS防注入

--- common/src/main/java/com/eryansky/common/utils/encode/Encryption.java
+++ common/src/main/java/com/eryansky/common/utils/encode/Encryption.java
@@ -12,6 +12,7 @@ import java.security.Key;
 /**
  * DES对称加密/解密工具类.
  * <br>加密加密字符串采用DEFAULT_CHARSET编码（UTF-8）.
+ *
  * @author 尔演&Eryan eryanwcp@gmail.com
  * @date 2011-12-27 上午9:48:31
  */
@@ -29,11 +30,9 @@ public class Encryption {
      * 将byte数组转换为表示16进制值的字符串， 如：byte[]{8,18}转换为：0813， 和public static byte[]
      * hexStr2ByteArr(String strIn) 互为可逆的转换过程
      *
-     * @param arrB
-     *            需要转换的byte数组
+     * @param arrB 需要转换的byte数组
      * @return 转换后的字符串
-     * @throws Exception
-     *             本方法不处理任何异常，所有异常全部抛出
+     * @throws Exception 本方法不处理任何异常，所有异常全部抛出
      */
     private static String byteArr2HexStr(byte[] arrB) throws Exception {
         int iLen = arrB.length;
@@ -58,12 +57,9 @@ public class Encryption {
      * 将表示16进制值的字符串转换为byte数组， 和public static String byteArr2HexStr(byte[] arrB)
      * 互为可逆的转换过程
      *
-     * @param strIn
-     *            需要转换的字符串
+     * @param strIn 需要转换的字符串
      * @return 转换后的byte数组
-     * @throws Exception
-     *             本方法不处理任何异常，所有异常全部抛出
-     *
+     * @throws Exception 本方法不处理任何异常，所有异常全部抛出
      */
     private static byte[] hexStr2ByteArr(String strIn) throws Exception {
         byte[] arrB = strIn.getBytes();
@@ -81,8 +77,7 @@ public class Encryption {
     /**
      * 加密字节数组
      *
-     * @param arrB
-     *            需加密的字节数组
+     * @param arrB 需加密的字节数组
      * @return 加密后的字节数组
      * @throws Exception
      */
@@ -93,7 +88,7 @@ public class Encryption {
     /**
      * 加密字节数组
      *
-     * @param arrB 需加密的字节数组
+     * @param arrB   需加密的字节数组
      * @param ketStr 密钥
      * @return 加密后的字节数组
      * @throws Exception
@@ -108,8 +103,7 @@ public class Encryption {
     /**
      * 加密字符串 字符串采用默认编码DEFAULT_CHARSET（UTF-8）
      *
-     * @param strIn
-     *            需加密的字符串
+     * @param strIn 需加密的字符串
      * @return 加密后的字符串
      * @throws Exception
      */
@@ -118,11 +112,24 @@ public class Encryption {
         return byteArr2HexStr(encrypt(strIn.getBytes(DEFAULT_CHARSET)));
     }
 
+
+    /**
+     * 加密字符串 字符串采用默认编码DEFAULT_CHARSET（UTF-8）
+     *
+     * @param strIn  需加密的字符串
+     * @param strKey 需加密的密钥
+     * @return 加密后的字符串
+     * @throws Exception
+     */
+    public static String encrypt(String strIn, String strKey) throws Exception {
+//		return byteArr2HexStr(encrypt(strIn.getBytes()));
+        return byteArr2HexStr(encrypt(strIn.getBytes(DEFAULT_CHARSET), strKey));
+    }
+
     /**
      * 解密字节数组
      *
-     * @param arrB
-     *            需解密的字节数组
+     * @param arrB 需解密的字节数组
      * @return 解密后的字节数组
      * @throws Exception
      */
@@ -133,7 +140,7 @@ public class Encryption {
     /**
      * 解密字节数组
      *
-     * @param arrB 需解密的字节数组
+     * @param arrB   需解密的字节数组
      * @param keyStr 密钥
      * @return 解密后的字节数组
      * @throws Exception
@@ -148,8 +155,7 @@ public class Encryption {
     /**
      * 解密字符串 字符串采用默认编码DEFAULT_CHARSET（UTF-8）
      *
-     * @param strIn
-     *            需解密的字符串
+     * @param strIn 需解密的字符串
      * @return 解密后的字符串
      * @throws Exception
      */
@@ -158,10 +164,21 @@ public class Encryption {
     }
 
     /**
+     * 解密字符串 字符串采用默认编码DEFAULT_CHARSET（UTF-8）
+     *
+     * @param strIn  需解密的字符串
+     * @param strKey 需解密的密钥
+     * @return 解密后的字符串
+     * @throws Exception
+     */
+    public static String decrypt(String strIn, String strKey) throws Exception {
+        return new String(decrypt(hexStr2ByteArr(strIn), strKey), DEFAULT_CHARSET);
+    }
+
+    /**
      * 从指定字符串生成密钥，密钥所需的字节数组长度为8位 不足8位时后面补0，超出8位只取前8位
      *
-     * @param arrBTmp
-     *            构成该字符串的字节数组
+     * @param arrBTmp 构成该字符串的字节数组
      * @return 生成的密钥
      * @throws Exception
      */

f6519a628feeb49f3263553dfc53ff78e395227b
eryan <eryanwcp@gmail.com>
2021-12-01 16:20:34 +0800

    20211129 1、动态加密登录密码 2、XSS防注入

--- app-common/src/main/java/com/eryansky/modules/notice/web/NoticeController.java
+++ app-common/src/main/java/com/eryansky/modules/notice/web/NoticeController.java
@@ -46,6 +46,7 @@ import com.eryansky.utils.SelectType;
 import org.apache.commons.fileupload.FileUploadBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -293,6 +294,10 @@ public class NoticeController extends SimpleController {
         return JsonMapper.getInstance().toJson(list, User.class, new String[]{"id", "name", "defaultOrganName"});
     }
 
+    @Override
+    protected void initBinder(WebDataBinder binder) {
+        super.defaultWebDataBinder(binder);
+    }
 
     /**
      * 保存

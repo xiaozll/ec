ae5ec6c1aad1c907d5d9b9f598d4717e3fc18da7
eryan <eryanwcp@gmail.com>
2021-12-01 08:57:31 +0800

    20211129 1、动态加密登录密码 2、XSS防注入

--- app-common/src/main/java/com/eryansky/core/security/CustomDruidPasswordCallback.java
+++ app-common/src/main/java/com/eryansky/core/security/CustomDruidPasswordCallback.java
@@ -21,17 +21,21 @@ public class CustomDruidPasswordCallback extends DruidPasswordCallback {
     @Override
     public void setProperties(Properties properties) {
         super.setProperties(properties);
+        String cdecrypt = (String) properties.get("config.decrypt");
+        boolean decrypt = Boolean.parseBoolean(cdecrypt);
         //获取配置文件中的已经加密的密码
         String ePassword = (String) properties.get("password");
         String cKey = (String) properties.get("key");
-        if (StringUtils.isNotEmpty(ePassword)) {
-            try {
-                //这里的代码是将密码进行解密，并设置
-                String password = StringUtils.isNotBlank(cKey) ?  Encryption.decrypt(ePassword,cKey ): Encryption.decrypt(ePassword);
-                setPassword(password.toCharArray());
-            } catch (Exception e) {
-                logger.error(e.getMessage(), e);
-            }
+        if (!decrypt || !StringUtils.isNotEmpty(ePassword)) {
+            return;
+        }
+        try {
+            //这里的代码是将密码进行解密，并设置
+            String password = StringUtils.isNotBlank(cKey) ?  Encryption.decrypt(ePassword,cKey ): Encryption.decrypt(ePassword);
+            setPassword(password.toCharArray());
+            logger.debug("数据库密码加密参数转换成功.");
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
     }
 

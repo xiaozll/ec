2579e36993d6f796b1cfb40ed5704481aab21b88
eryan <eryanwcp@gmail.com>
2022-05-07 16:50:16 +0800

    comment

--- common/src/main/java/com/eryansky/common/utils/DateUtils.java
+++ common/src/main/java/com/eryansky/common/utils/DateUtils.java
@@ -1482,8 +1482,46 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
             throws ParseException {
         return strDateDiffTimes(time, sec, 's');
     }
-	
-	/**
+
+    /**
+     * 计算当前月有多少天
+     *
+     * @param year
+     * @param month
+     * @return
+     */
+    public int getMonthTotalDays(int year, int month) {
+        int days = 0;
+        if (month != 2) {
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    days = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+
+            }
+        } else {
+            // 闰年
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                days = 29;
+            else
+                days = 28;
+        }
+        return days;
+    }
+
+
+    /**
 	 * @param args
 	 * @throws java.text.ParseException
 	 */

418f998edea5478aaa47fe13bbce5d0caadc7ce8
eryan <eryanwcp@gmail.com>
2022-11-09 09:10:06 +0800

    spring boot 2.7.5 add organ is_leaf

--- app-common/src/main/java/com/eryansky/modules/sys/web/UserController.java
+++ app-common/src/main/java/com/eryansky/modules/sys/web/UserController.java
@@ -106,7 +106,15 @@ public class UserController extends SimpleController {
         Page<User> page = new Page<>(SpringMVCHolder.getRequest());
         SessionInfo sessionInfo = SecurityUtils.getCurrentSessionInfo();
         if (StringUtils.isBlank(organId)) {
-            organId = (sessionInfo.isSuperUser() || SecurityUtils.isPermittedMaxRoleDataScope() || SecurityUtils.isPermittedDataScope(sessionInfo.getUserId(),DataScope.HOME_COMPANY_AND_CHILD.getValue())) ? sessionInfo.getLoginHomeCompanyId():sessionInfo.getLoginOrganId();
+            if(sessionInfo.isSuperUser() || SecurityUtils.isPermittedMaxRoleDataScope()){
+                organId = sessionInfo.getLoginHomeCompanyId();
+            }else if(SecurityUtils.isPermittedDataScope(sessionInfo.getUserId(),DataScope.HOME_COMPANY_AND_CHILD.getValue()) || SecurityUtils.isPermittedDataScope(sessionInfo.getUserId(),DataScope.HOME_COMPANY.getValue())){
+                organId = sessionInfo.getLoginHomeCompanyId();
+            }else if(SecurityUtils.isPermittedDataScope(sessionInfo.getUserId(),DataScope.COMPANY_AND_CHILD.getValue()) || SecurityUtils.isPermittedDataScope(sessionInfo.getUserId(),DataScope.COMPANY.getValue())){
+                organId = sessionInfo.getLoginCompanyId();
+            }else{
+                organId = sessionInfo.getLoginOrganId();
+            }
         }
 
         page = userService.findPage(page, organId, query, userType);

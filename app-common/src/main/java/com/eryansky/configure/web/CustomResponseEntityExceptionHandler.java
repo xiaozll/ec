cb294f2fdfbfeadf1ddf5cfa83fc4a68346aa971
eryan <eryanwcp@gmail.com>
2023-05-17 12:21:19 +0800

    自定义 handleHttpRequestMethodNotSupported

--- app-common/src/main/java/com/eryansky/configure/web/CustomResponseEntityExceptionHandler.java
+++ app-common/src/main/java/com/eryansky/configure/web/CustomResponseEntityExceptionHandler.java
@@ -1,17 +1,20 @@
 package com.eryansky.configure.web;
 
+import com.eryansky.common.utils.mapper.JsonMapper;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -20,11 +23,17 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
                                                                          HttpHeaders headers, HttpStatus status, WebRequest request) {
         String method = ex.getMethod();
-        String[] supportedMethods = ex.getSupportedMethods();
-        String body = "不支持的请求类型：" + method + "，支持的请求类型：" + ((ServletWebRequest) request).getRequest().getRequestURI() + " - "+ Arrays.toString(supportedMethods);
+        Set<HttpMethod> supportedHttpMethods = ex.getSupportedHttpMethods();
+        String body = ((ServletWebRequest) request).getRequest().getRequestURI() + " 不支持的请求类型：" + method + "，支持的请求类型：" + JsonMapper.toJsonString(supportedHttpMethods);
         logger.error(body);
         Map<String, Object> map = new HashMap<>();
         map.put("body", body);
+
+        pageNotFoundLogger.warn(ex.getMessage());
+
+        if (!CollectionUtils.isEmpty(supportedHttpMethods)) {
+            headers.setAllow(supportedHttpMethods);
+        }
         return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 

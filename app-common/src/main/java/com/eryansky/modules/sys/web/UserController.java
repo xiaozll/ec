f7317f513ea55d3a5c12e1321ae82024d3c907a6
eryan <eryanwcp@gmail.com>
2023-05-29 12:16:18 +0800

    V4.2.20230521.03 1、修复部分URL垂直越权漏洞,用户管理增加功能权限：数据查看“sys:user:dataView” /a/sys/user/organUserTree /a/sys/user/customUserList 2、修复部分请求错误页面信息泄露 server.tomcat.relaxed-query-chars=|,{,},[,],\,

--- app-common/src/main/java/com/eryansky/modules/sys/web/UserController.java
+++ app-common/src/main/java/com/eryansky/modules/sys/web/UserController.java
@@ -555,6 +555,7 @@ public class UserController extends SimpleController {
      * @param query
      * @return
      */
+    @RequiresPermissions(logical = Logical.OR,value = {"sys:user:edit","sys:user:view","sys:user:dataView"})
     @PostMapping(value = {"customUserList"})
     @ResponseBody
     public String userList(String dataScope,
@@ -676,6 +677,7 @@ public class UserController extends SimpleController {
      * @param cascade
      * @return
      */
+    @RequiresPermissions(logical = Logical.OR,value = {"sys:user:edit","sys:user:view","sys:user:dataView"})
     @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST},value = {"organUserTree"})
     @ResponseBody
     public List<TreeNode> organUserTree(String parentId,

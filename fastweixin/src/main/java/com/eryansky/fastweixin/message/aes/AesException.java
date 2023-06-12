3c2c6f65d7bd6ca7e886a4bc086cec855eb4a5ad
eryan <eryanwcp@gmail.com>
2023-06-12 15:59:31 +0800

    还原weixin加密方法

--- fastweixin/src/main/java/com/eryansky/fastweixin/message/aes/AesException.java
+++ fastweixin/src/main/java/com/eryansky/fastweixin/message/aes/AesException.java
@@ -3,25 +3,20 @@ package com.eryansky.fastweixin.message.aes;
 @SuppressWarnings("serial")
 public class AesException extends Exception {
 
-    public final static int OK                     = 0;
+    public final static int OK = 0;
     public final static int ValidateSignatureError = -40001;
-    public final static int ParseXmlError          = -40002;
-    public final static int ComputeSignatureError  = -40003;
-    public final static int IllegalAesKey          = -40004;
-    public final static int ValidateAppidError     = -40005;
-    public final static int EncryptAESError        = -40006;
-    public final static int DecryptAESError        = -40007;
-    public final static int IllegalBuffer          = -40008;
+    public final static int ParseXmlError = -40002;
+    public final static int ComputeSignatureError = -40003;
+    public final static int IllegalAesKey = -40004;
+    public final static int ValidateCorpidError = -40005;
+    public final static int EncryptAESError = -40006;
+    public final static int DecryptAESError = -40007;
+    public final static int IllegalBuffer = -40008;
     //public final static int EncodeBase64Error = -40009;
     //public final static int DecodeBase64Error = -40010;
     //public final static int GenReturnXmlError = -40011;
 
-    private final int code;
-
-    AesException(int code) {
-        super(getMessage(code));
-        this.code = code;
-    }
+    private int code;
 
     private static String getMessage(int code) {
         switch (code) {
@@ -33,8 +28,8 @@ public class AesException extends Exception {
                 return "sha加密生成签名失败";
             case IllegalAesKey:
                 return "SymmetricKey非法";
-            case ValidateAppidError:
-                return "appid校验失败";
+            case ValidateCorpidError:
+                return "corpid校验失败";
             case EncryptAESError:
                 return "aes加密失败";
             case DecryptAESError:
@@ -54,6 +49,11 @@ public class AesException extends Exception {
 
     public int getCode() {
         return code;
+    }
+
+    AesException(int code) {
+        super(getMessage(code));
+        this.code = code;
     }
 
 }
--- fastweixin/src/main/java/com/eryansky/fastweixin/message/aes/WXBizMsgCrypt.java
+++ fastweixin/src/main/java/com/eryansky/fastweixin/message/aes/WXBizMsgCrypt.java
@@ -13,32 +13,34 @@
  */
 package com.eryansky.fastweixin.message.aes;
 
-import org.apache.commons.codec.binary.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.Random;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 提供接收和推送给公众平台消息的加解密接口(UTF8编码的字符串).
+ * 提供接收和推送给企业微信消息的加解密接口(UTF8编码的字符串).
  * <ol>
- * <li>第三方回复加密消息给公众平台</li>
- * <li>第三方收到公众平台发送的消息，验证消息的安全性，并对消息进行解密。</li>
+ * 	<li>第三方回复加密消息给企业微信</li>
+ * 	<li>第三方收到企业微信发送的消息，验证消息的安全性，并对消息进行解密。</li>
  * </ol>
  * 说明：异常java.security.InvalidKeyException:illegal Key Size的解决方案
  * <ol>
- * <li>在官方网站下载JCE无限制权限策略文件（JDK7的下载地址：
- * http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html</li>
- * <li>下载后解压，可以看到local_policy.jar和US_export_policy.jar以及readme.txt</li>
- * <li>如果安装了JRE，将两个jar文件放到%JRE_HOME%\lib\security目录下覆盖原来的文件</li>
- * <li>如果安装了JDK，将两个jar文件放到%JDK_HOME%\jre\lib\security目录下覆盖原来文件</li>
+ * 	<li>在官方网站下载JCE无限制权限策略文件（JDK7的下载地址：
+ *      http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html</li>
+ * 	<li>下载后解压，可以看到local_policy.jar和US_export_policy.jar以及readme.txt</li>
+ * 	<li>如果安装了JRE，将两个jar文件放到%JRE_HOME%\lib\security目录下覆盖原来的文件</li>
+ * 	<li>如果安装了JDK，将两个jar文件放到%JDK_HOME%\jre\lib\security目录下覆盖原来文件</li>
  * </ol>
  */
 public class WXBizMsgCrypt implements AutoCloseable {
@@ -49,23 +51,24 @@ public class WXBizMsgCrypt implements AutoCloseable {
     Base64 base64 = new Base64();
     byte[] aesKey;
     String token;
-    String appId;
+    String receiveid;
     private static final SecureRandom random = new SecureRandom();
+
     /**
      * 构造函数
+     * @param token 企业微信后台，开发者设置的token
+     * @param encodingAesKey 企业微信后台，开发者设置的EncodingAESKey
+     * @param receiveid, 不同场景含义不同，详见文档
      *
-     * @param token          公众平台上，开发者设置的token
-     * @param encodingAesKey 公众平台上，开发者设置的EncodingAESKey
-     * @param appId          公众平台appid
      * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
      */
-    public WXBizMsgCrypt(String token, String encodingAesKey, String appId) throws AesException {
+    public WXBizMsgCrypt(String token, String encodingAesKey, String receiveid) throws AesException {
         if (encodingAesKey.length() != 43) {
             throw new AesException(AesException.IllegalAesKey);
         }
 
         this.token = token;
-        this.appId = appId;
+        this.receiveid = receiveid;
         aesKey = Base64.decodeBase64(encodingAesKey + "=");
     }
 
@@ -112,13 +115,13 @@ public class WXBizMsgCrypt implements AutoCloseable {
         byte[] randomStrBytes = randomStr.getBytes(CHARSET);
         byte[] textBytes = text.getBytes(CHARSET);
         byte[] networkBytesOrder = getNetworkBytesOrder(textBytes.length);
-        byte[] appidBytes = appId.getBytes(CHARSET);
+        byte[] receiveidBytes = receiveid.getBytes(CHARSET);
 
-        // randomStr + networkBytesOrder + text + appid
+        // randomStr + networkBytesOrder + text + receiveid
         byteCollector.addBytes(randomStrBytes);
         byteCollector.addBytes(networkBytesOrder);
         byteCollector.addBytes(textBytes);
-        byteCollector.addBytes(appidBytes);
+        byteCollector.addBytes(receiveidBytes);
 
         // ... + pad: 使用自定义的填充方式对明文进行补位填充
         byte[] padBytes = PKCS7Encoder.encode(byteCollector.size());
@@ -173,43 +176,44 @@ public class WXBizMsgCrypt implements AutoCloseable {
             throw new AesException(AesException.DecryptAESError);
         }
 
-        String xmlContent, from_appid;
+        String xmlContent, from_receiveid;
         try {
             // 去除补位字符
             byte[] bytes = PKCS7Encoder.decode(original);
 
-            // 分离16位随机字符串,网络字节序和AppId
+            // 分离16位随机字符串,网络字节序和receiveid
             byte[] networkOrder = Arrays.copyOfRange(bytes, 16, 20);
 
             int xmlLength = recoverNetworkBytesOrder(networkOrder);
 
             xmlContent = new String(Arrays.copyOfRange(bytes, 20, 20 + xmlLength), CHARSET);
-            from_appid = new String(Arrays.copyOfRange(bytes, 20 + xmlLength, bytes.length),
+            from_receiveid = new String(Arrays.copyOfRange(bytes, 20 + xmlLength, bytes.length),
                     CHARSET);
         } catch (Exception e) {
             logger.error(e.getMessage(),e);
             throw new AesException(AesException.IllegalBuffer);
         }
 
-        // appid不相同的情况
-        if (!from_appid.equals(appId)) {
-            throw new AesException(AesException.ValidateAppidError);
+        // receiveid不相同的情况
+        if (!from_receiveid.equals(receiveid)) {
+            throw new AesException(AesException.ValidateCorpidError);
         }
         return xmlContent;
 
     }
 
     /**
-     * 将公众平台回复用户的消息加密打包.
+     * 将企业微信回复用户的消息加密打包.
      * <ol>
-     * <li>对要发送的消息进行AES-CBC加密</li>
-     * <li>生成安全签名</li>
-     * <li>将消息密文和安全签名打包成xml格式</li>
+     * 	<li>对要发送的消息进行AES-CBC加密</li>
+     * 	<li>生成安全签名</li>
+     * 	<li>将消息密文和安全签名打包成xml格式</li>
      * </ol>
      *
-     * @param replyMsg  公众平台待回复用户的消息，xml格式的字符串
+     * @param replyMsg 企业微信待回复用户的消息，xml格式的字符串
      * @param timeStamp 时间戳，可以自己生成，也可以用URL参数的timestamp
-     * @param nonce     随机串，可以自己生成，也可以用URL参数的nonce
+     * @param nonce 随机串，可以自己生成，也可以用URL参数的nonce
+     *
      * @return 加密后的可以直接回复用户的密文，包括msg_signature, timestamp, nonce, encrypt的xml格式的字符串
      * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
      */
@@ -233,15 +237,16 @@ public class WXBizMsgCrypt implements AutoCloseable {
     /**
      * 检验消息的真实性，并且获取解密后的明文.
      * <ol>
-     * <li>利用收到的密文生成安全签名，进行签名验证</li>
-     * <li>若验证通过，则提取xml中的加密消息</li>
-     * <li>对消息进行解密</li>
+     * 	<li>利用收到的密文生成安全签名，进行签名验证</li>
+     * 	<li>若验证通过，则提取xml中的加密消息</li>
+     * 	<li>对消息进行解密</li>
      * </ol>
      *
      * @param msgSignature 签名串，对应URL参数的msg_signature
-     * @param timeStamp    时间戳，对应URL参数的timestamp
-     * @param nonce        随机串，对应URL参数的nonce
-     * @param postData     密文，对应POST请求的数据
+     * @param timeStamp 时间戳，对应URL参数的timestamp
+     * @param nonce 随机串，对应URL参数的nonce
+     * @param postData 密文，对应POST请求的数据
+     *
      * @return 解密后的原文
      * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
      */
@@ -269,11 +274,11 @@ public class WXBizMsgCrypt implements AutoCloseable {
 
     /**
      * 验证URL
-     *
      * @param msgSignature 签名串，对应URL参数的msg_signature
-     * @param timeStamp    时间戳，对应URL参数的timestamp
-     * @param nonce        随机串，对应URL参数的nonce
-     * @param echoStr      随机串，对应URL参数的echostr
+     * @param timeStamp 时间戳，对应URL参数的timestamp
+     * @param nonce 随机串，对应URL参数的nonce
+     * @param echoStr 随机串，对应URL参数的echostr
+     *
      * @return 解密之后的echostr
      * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
      */

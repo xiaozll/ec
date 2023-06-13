a7e3e69088f3417e6d067383aad48f20e120c369
eryan <eryanwcp@gmail.com>
2023-06-13 16:36:09 +0800

    还原weixin加密方法

--- fastweixin/src/main/java/com/eryansky/fastweixin/servlet/QYWeixinControllerSupport.java
+++ fastweixin/src/main/java/com/eryansky/fastweixin/servlet/QYWeixinControllerSupport.java
@@ -5,9 +5,7 @@ import com.eryansky.fastweixin.message.aes.WXBizMsgCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -33,8 +31,14 @@ public abstract class QYWeixinControllerSupport extends QYWeixinSupport {
      */
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    protected final String bind(HttpServletRequest request){
-        return legalStr(request);
+    protected final String bind(HttpServletRequest request,
+                                HttpServletResponse response,
+                                @RequestParam(value = "msg_signature", required = false) String msgSignature,
+                                @RequestParam(value = "nonce", required = false) String nonce,
+                                @RequestParam(value = "echostr", required = false) String echoStr,
+                                @RequestParam(value = "timestamp", required = false) String timestamp,
+                                @RequestBody(required = false) String xml){
+        return legalStr(request,msgSignature,nonce,timestamp,echoStr);
     }
 
     /**
@@ -46,7 +50,13 @@ public abstract class QYWeixinControllerSupport extends QYWeixinSupport {
      */
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    protected final String process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected final String process(HttpServletRequest request,
+                                   HttpServletResponse response,
+                                   @RequestParam(value = "msg_signature", required = false) String msgSignature,
+                                   @RequestParam(value = "nonce", required = false) String nonce,
+                                   @RequestParam(value = "echostr", required = false) String echoStr,
+                                   @RequestParam(value = "timestamp", required = false) String timestamp,
+                                   @RequestBody(required = false) String xml) throws ServletException, IOException{
 //        if(StrUtil.isBlank(legalStr(request))){
 //            return "";
 //        }
@@ -61,16 +71,16 @@ public abstract class QYWeixinControllerSupport extends QYWeixinSupport {
      * @param request
      * @return
      */
-    protected String legalStr(HttpServletRequest request){
-        String echoStr = "";
+    protected String legalStr(HttpServletRequest request,String msgSignature,String nonce,String timestamp,String echoStr){
+        String _echoStr = "";
         if(StrUtil.isBlank(getToken()) || StrUtil.isBlank(getAESKey()) || StrUtil.isBlank(getCropId())){
-            return echoStr;
+            return _echoStr;
         }
         try (WXBizMsgCrypt pc = new WXBizMsgCrypt(getToken(), getAESKey(), getCropId())){
-            echoStr = pc.verifyUrl(request.getParameter("msg_signature"), request.getParameter("timestamp"), request.getParameter("nonce"), request.getParameter("echostr"));
+            _echoStr = pc.verifyUrl(msgSignature, timestamp, nonce, echoStr);
         } catch (Exception e) {
             LOG.error(e.getMessage(),e);
         }
-        return echoStr;
+        return _echoStr;
     }
 }
